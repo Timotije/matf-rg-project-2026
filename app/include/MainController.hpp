6a44b35a3dd53a8c0a9b451accec517c5ff9cdc4
Timotije <timotije.tuf@gmail.com>
2026-08-09 14:09:29 +0200

    Basic implementation

---
+++ app/include/MainController.hpp
@@ -0,0 +1,18 @@
+#ifndef MATF_RG_PROJECT_MAINCONTROLLER_HPP
+#define MATF_RG_PROJECT_MAINCONTROLLER_HPP
+#include <engine/core/Controller.hpp>
+
+namespace app {
+    class MainController : public engine::core::Controller {
+        void initialize() override;
+
+        bool loop() override;
+
+        public:
+            std::string_view name() const override {
+                return "app::MainController";
+            };
+    };
+}// namespace app
+
+#endif//MATF_RG_PROJECT_MAINCONTROLLER_HPP
