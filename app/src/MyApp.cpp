d2065c6bc8b9ee3c0df6cd8269151bde32805cc8
Timotije <timotije.tuf@gmail.com>
2026-08-10 23:51:53 +0200

    Implement basic GUI

--- app/src/MyApp.cpp
+++ app/src/MyApp.cpp
@@ -1,5 +1,6 @@
 #include "../include/MyApp.hpp"
 
+#include <GuiController.hpp>
 #include <MainController.hpp>
 #include <spdlog/spdlog.h>
 
@@ -7,6 +8,8 @@ namespace app {
     void MyApp::app_setup() {
         spdlog::info("App setup complete");
         auto main_controller = register_controller<app::MainController>();
+        auto gui_controller = register_controller<app::GUIController>();
         main_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
+        main_controller->before(gui_controller);
     }
 }// namespace app
